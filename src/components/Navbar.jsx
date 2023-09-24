5a1bbcd3fa0617a1a950263d696556d60e4d53f9
m zain ali <mzain42401@gmail.com>
2023-09-24 13:03:28 +0500

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { MenuOutlined, CloseOutlined } from '@ant-design/icons';
+import './navbar.css'
 
 const Navbar = () => {
+  let [initial, setInitial] = useState(true)
+  let navRef = useRef()
+
+  const toggleIcons = () => {
+    setInitial(!initial)
+    showNav()
+  }
+
+
+  const showNav = () => {
+    navRef.current.classList.toggle('active')
+  }
   return (
     <>
-      <nav className='fixed w-full flex  justify-between bg-indigo-600 h-16 text-white items-center px-3'>
+      <nav   className=' navbar fixed w-full flex  justify-between bg-indigo-600 h-16 text-white items-center px-3'>
         <div className='text-3xl'>Shopping</div>
-<ul className='flex justify-between items-center'>
+<ul ref={navRef} className='flex justify-between items-center'>
 <Link to='/'> <li className='mx-5'> HOME</li></Link>
 <Link to='/cart'><li className='mx-5'>  CART</li></Link>
 <Link to='/post'>  <li className='mx-5 text-indigo-600 border-2 border-white border-solid	 bg-white px-6 py-1 rounded-full hover:bg-indigo-600 hover:text-white hover:cursor-pointer'>  PRODUCT </li></Link>
     
 </ul>
+<div className='navToggleBtn' onClick={toggleIcons}>
+          {initial ? <MenuOutlined /> : <CloseOutlined />}
+        </div>
       </nav>
     </>
   )
